d6a91dd9208cae31062e0f8f53c3baceaa65e3a4
Lucass2160 <lucas-cali@hotmail.com>
2023-07-20 16:06:30 -0300

    paginado tiene un bug

--- src/components/pagination/Pagination.jsx
+++ src/components/pagination/Pagination.jsx
@@ -18,15 +18,31 @@ function Pagination({ pages = 10, setCurrentPage }) {
 
     if (numberOfPages.length < 6) {
       tempNumberOfPages = numberOfPages;
-    } else if (currentButton <= 3) {
-      tempNumberOfPages = [...numberOfPages.slice(0, 5), dotsRight, numberOfPages.length];
+    } else if (currentButton >= 1 && currentButton <= 3) {
+      tempNumberOfPages = [1, 2, 3, 4, dotsInitial, numberOfPages.length];
     } else if (currentButton === 4) {
-      tempNumberOfPages = [1, 2, 3, 4, 5, dotsRight, numberOfPages.length];
+      const sliced = numberOfPages.slice(0, 5);
+      tempNumberOfPages = [...sliced, dotsInitial, numberOfPages.length];
     } else if (currentButton > 4 && currentButton < numberOfPages.length - 2) {
-      const sliced = numberOfPages.slice(currentButton - 2, currentButton + 1);
-      tempNumberOfPages = [1, dotsLeft, ...sliced, dotsRight, numberOfPages.length];
-    } else if (currentButton >= numberOfPages.length - 2) {
-      tempNumberOfPages = [1, dotsLeft, ...numberOfPages.slice(numberOfPages.length - 4)];
+      const sliced1 = numberOfPages.slice(currentButton - 2, currentButton);
+      const sliced2 = numberOfPages.slice(currentButton, currentButton + 1);
+      tempNumberOfPages = [
+        1,
+        dotsLeft,
+        ...sliced1,
+        ...sliced2,
+        dotsRight,
+        numberOfPages.length,
+      ];
+    } else if (currentButton > numberOfPages.length - 3) {
+      const sliced = numberOfPages.slice(numberOfPages.length - 4);
+      tempNumberOfPages = [1, dotsLeft, ...sliced];
+    } else if (currentButton === dotsInitial) {
+      setCurrentButton(arrOfCurrButtons[arrOfCurrButtons.length - 3] + 1);
+    } else if (currentButton === dotsRight) {
+      setCurrentButton(arrOfCurrButtons[3] + 2);
+    } else if (currentButton === dotsLeft) {
+      setCurrentButton(arrOfCurrButtons[3] - 2);
     }
 
     setArrOfCurrButtons(tempNumberOfPages);
