c03a39bed33cf408f38018a650e763bd3f2e7917
Mariano Menéndez <mariano.menendez96@gmail.com>
2023-07-21 14:58:37 -0300

    filtroName y Category

--- src/components/cardsContainer/CardsContainer.jsx
+++ src/components/cardsContainer/CardsContainer.jsx
@@ -2,13 +2,33 @@ import { useEffect, useState } from "react"
 import CardElement from "../cardsElement/CardElement"
 import {useSelector} from "react-redux"
 import { useLocation } from "react-router-dom"
+import CategoryList from "../categoryList/categoryList"
 
 export default function CardsContainer({products}) {
-    
-    
+    const [catFilter, setCatFilter] = useState("")
+    const [productList, setProductList] = useState([])
+    const location = useLocation()
+    useEffect(()=> {
+    if(catFilter ==="") {
+    setProductList(products)
+    }
+    else{
+    setProductList(products.filter(product => product.category === catFilter))
+    console.log(productList)
+    }}
+    ,[catFilter])
     return(
         <div>
-            {products?.map(product => <CardElement product={product} />)}
+            {location.pathname === "/" ? 
+                <>
+                <CategoryList products={products} setCatFilter={setCatFilter}/>
+                {productList?.map(product => <CardElement product={product}  />)}
+                </>
+                :
+                <>
+                {products?.map(product => <CardElement product={product} />)}
+                </>
+            }
         </div>
     )
 }
